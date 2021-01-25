4598bce544c20ae58639db0e941e5e84d1596e93
Avika-Coder <76557800+Avika-Coder@users.noreply.github.com>
2021-01-25 15:04:42 +0530

    Update scriptIplResults.js

--- scriptIplResults.js
+++ scriptIplResults.js
@@ -1,11 +1,11 @@
-var firebaseConfig = {
-    apiKey: "AIzaSyChtuGMP3KzZBP0DhAjOprGfn5CNurRyn4",
-    authDomain: "iplresults-9ef77.firebaseapp.com",
-    databaseURL: "https://iplresults-9ef77.firebaseio.com",
-    projectId: "iplresults-9ef77",
-    storageBucket: "iplresults-9ef77.appspot.com",
-    messagingSenderId: "533733582731",
-    appId: "1:533733582731:web:c5c9f9d7e2f46c3767318e"
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyCipENapidOJolMQoZXi5ZInMMf6sPp9To",
+    authDomain: "ipl-result.firebaseapp.com",
+    projectId: "ipl-result",
+    storageBucket: "ipl-result.appspot.com",
+    messagingSenderId: "58034788847",
+    appId: "1:58034788847:web:f4b1e7e907efe5cece7e1a"
   };
+  // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
-
